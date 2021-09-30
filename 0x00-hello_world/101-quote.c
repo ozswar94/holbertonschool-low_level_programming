7f2278596cc499fc1070e218bfc4f662a7a1c6ec
ozswar94 <3630@holbertonschool.com>
2021-09-30 10:49:47 +0000

    finish task 9

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,30 @@
+#include <unistd.h>
+
+/**
+* print - Print string on ouput (terminal)
+*
+* @str: The string which will be display
+*/
+void print(char *str)
+{
+	while (*str)
+	{
+		write(1, str, 1);
+		str++;
+	}
+}
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	print(str);
+	return (0);
+}
+
