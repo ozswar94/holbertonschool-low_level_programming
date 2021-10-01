21e98a67254a30256e07180c98cfe40702f17caa
Jordan Mpounza <3630@holbertonschool.com>
2021-10-01 14:32:08 +0000

    task 12 finish

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - Entry of programe
+* Return: 0 Always (success)
+*/
+int main(void)
+{
+	int nb1;
+	int nb2;
+
+	for (nb1 = 0; nb1 < 100; nb1++)
+	{
+		for (nb2 = 0; nb2 < 100; nb2++)
+		{
+			if (nb1 > nb2)
+			{
+				putchar((nb1 / 10 ) + '0');
+				putchar((nb1 % 10 ) + '0');
+				putchar(' ');
+				putchar((nb2 / 10) + '0');
+				putchar((nb2 % 10) + '0');
+				if (nb1 < 99 || nb2 < 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
