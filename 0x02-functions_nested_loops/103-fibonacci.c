e3b4ab8f39dfea0f46cc97568800c8a6e964ea6c
ozswar94 <3630@holbertonschool.com>
2021-10-05 21:05:58 +0000

    fix the condition

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,22 +9,22 @@
 int main(void)
 {
 	long fib[3];
+	long result;
 	int i;
 
 	for (i = 0; i < 3; i++)
 		fib[i] = 1;
+	result = 0;
 	while (fib[0] < LIMIT)
 	{
 		if (fib[0] % 2 == 0)
 		{
-			if (i != LIMIT - 1)
-				printf("%ld, ", fib[0]);
-			else
-				printf("%ld\n", fib[0]);
-        }
+			result += fib[0];
+		}
 		fib[0] = fib[0] + fib[1];
 		fib[1] = fib[2];
 		fib[2] = fib[0];
 	}
+	printf("%ld\n", result);
 	return (0);
 }
