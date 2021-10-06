8db707578c76afc2feb2242dd8f750b34c557033
Jordan Mpounza <3630@holbertonschool.com>
2021-10-06 12:52:29 +0000

    style betty

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,7 +12,7 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b && a > c)
+	if (a > b && b > c)
 	{
 		largest = a;
 	}
@@ -20,9 +20,12 @@ int largest_number(int a, int b, int c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else
 	{
 		largest = c;
 	}
+
 	return (largest);
 }
+
+
