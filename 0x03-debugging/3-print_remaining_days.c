4663ab8df4dc08bb11e5a51c65f68a1bc53761ca
ozswar94 <3630@holbertonschool.com>
2021-10-07 16:36:01 +0000

    test task 3

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -14,34 +14,15 @@ void print_remaining_days(int month, int day, int year)
 {
 	if ((year % 4 == 0 || !(year % 100 == 0)) && (year % 400 == 0))
 	{
-		if (month == 2 && day > 60)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		else if (month == 4 && day > 120)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 90, year);
-		else if (month == 6 && day > 181)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 120, year);
-		else if (month == 9 && day > 273)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 212, year);
-		else if (month == 11 && day > 334)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 304, year);
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);
-		}
+		if (month > 2)
+			day++;
+		printf("Day of the year: %d\n", day);
+		printf("Remaining days: %d\n", 366 - day);
 	}
 	else
 	{
-		if (month == 2 && day >= 60)
+		if (month == 2 && day == 60)
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		else if (month == 4 && day >= 121)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 90, year);
-		else if (month == 6 && day >= 182)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 120, year);
-		else if (month == 9 && day >= 274)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 212, year);
-		else if (month == 11 && day >= 335)
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 304, year);
 		else
 		{
 			printf("Day of the year: %d\n", day);
