1f333b038d05881bd2911adcd7519743cb834b36
ozswar94 <3630@holbertonschool.com>
2021-10-07 11:12:57 +0000

    add task 100

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,13 +1,17 @@
 #include <stdio.h>
 #define FACT_N 612852475143
 
+/**
+* main - factor prime
+* Return: 0
+*/
 int main(void)
 {
 	unsigned long i;
 	unsigned long prime_f;
 
 	prime_f = 1;
-	for (i = 1; i <= FACT_N; i++)
+	for (i = 1; prime_f <= FACT_N; i++)
 		prime_f *= i;
 	printf("%lu\n", prime_f);
 	return (0);
