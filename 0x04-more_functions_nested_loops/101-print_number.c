6650d2da475887da001cd12c56d57154c6bc7e4d
ozswar94 <3630@holbertonschool.com>
2021-10-07 11:33:08 +0000

    add task 101

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* print_number - print number in output
+*
+* @n: integer
+* Return: nothing
+*/
+void print_number(int n)
+{
+	unsigned n_1;
+
+	if (n < 0)
+	{
+		n *= -1;
+		_putchar('-');
+	}
+	n_1 = n;
+	if (n_1 / 10)
+		print_number(n / 10);
+	_putchar((n % 10) + '0');
+}
