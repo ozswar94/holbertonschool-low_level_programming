59d5521343bf635130f4f239c69f838df7e0f2c6
ozswar94 <3630@holbertonschool.com>
2021-10-07 11:54:56 +0000

    hot fix algo iteration

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,7 +17,7 @@ void print_triangle(int size)
 	}
 	for (i = 0; i < size; i++)
 	{
-		for (j = 0; j < size - i; j++)
+		for (j = 1; j < size - i; j++)
 			_putchar(' ');
 		for (k = 0; k <= i; k++)
 			_putchar('#');
