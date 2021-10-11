88074add31d2969ea54e34d60b6d3eeef8762c11
ozswar94 <3630@holbertonschool.com>
2021-10-11 07:24:31 +0000

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+* reset_to_98 - reste a variable at 98
+* @n: integer pointer
+* Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef	MAIN_H
+#define	MAIN_H
+#include <unistd.h>
+
+void _putchar(char c);
+void reset_to_98(int *);
+
+#endif /* MAIN_H */
+
