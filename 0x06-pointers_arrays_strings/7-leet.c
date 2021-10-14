c493ed70ccd6b555bd6fcc4c368dce07e5401d59
Jordan Mpounza <3630@holbertonschool.com>
2021-10-14 10:08:15 +0000

    fix for the checker

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,23 +9,20 @@ char *leet(char *str)
 {
 	int i;
 	int j;
-	int k;
-	char *encode = "43071";
+	char *encode = "4433007711";
 	char *decode = "aAeEoOtTlL";
 
 	i = 0;
-	k = 0;
 	while (encode[i])
 	{
 		j = 0;
 		while (str[j])
 		{
-			if (str[j] == decode[i] || str[j] == decode[i + 1])
-				str[j] = encode[k];
+			if (str[j] == decode[i] || str[j] == decode[i])
+				str[j] = encode[i];
 			j++;
 		}
 		i += 2;
-		k++;
 	}
 	return (str);
 }
