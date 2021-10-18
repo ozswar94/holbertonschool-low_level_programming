273bf6fb35e3c6756ad49aa67453b86f1b382709
ozswar94 <3630@holbertonschool.com>
2021-10-18 20:19:56 +0000

    fix betty-style

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -15,11 +15,10 @@ void print_diagsums(int *a, int size)
 
 	sum = 0;
 	for (i = 0 ; i < size; i++)
-			sum += a[i + i * size];
-
+		sum += a[i + i * size];
 	printf("%d, ", sum);
 	sum = 0;
 	for (i = size - 1; i >= 0; i--)
-			sum += a[(size - i - 1) + i * size];
+		sum += a[(size - i - 1) + i * size];
 	printf("%d\n", sum);
 }
