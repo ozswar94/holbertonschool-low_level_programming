a9a665a971420943bfe2e715687dc5ef0d76f7e4
ozswar94 <3630@holbertonschool.com>
2021-10-18 14:40:05 +0000

    test 3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,12 +11,11 @@ char *_strchr(char *s, char c)
 	unsigned int i;
 
 	i = 0;
-	while (s[i])
-	{
-		if (c == s[i])
-			return (&s[i]);
+	while (s[i] && s[i] != c)
 		i++;
-	}
-	return (NULL);
+	if (s[i] == c)
+		return (&s[i])
+	else
+		return (NULL);
 }
 
