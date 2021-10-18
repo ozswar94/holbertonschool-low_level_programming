9fe23b79fb2b990b25f7df8c58e5548ea753df62
ozswar94 <3630@holbertonschool.com>
2021-10-18 14:18:26 +0000

    fix header file

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -15,31 +15,6 @@ int _isalpha(char c)
 */
 unsigned int _strspn(char *s, char *accept)
 {
-	int len_prefix;
-	int i;
-	int j;
-	int k;
-	int l;
 
-	i = len_prefix = 0;
-	while (s[i])
-	{
-		j = 0;
-		l = 0;
-		while (accept[j])
-		{
-			k = 0;
-			while (s[i + k])
-			{
-				if (s[i + k] == accept[j])
-				{
-					l++;
-					break;
-				}
-				k++;
-			}
-
-		}
-	}
 }
 
