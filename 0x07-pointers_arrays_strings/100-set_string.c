89a838118a0140cea9fab0326373b335a41612e4
ozswar94 <3630@holbertonschool.com>
2021-10-18 21:12:29 +0000

    test 1

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* set_string - print a chess board
+* @s:  pointer of pointer char
+* @to: pointer char
+* Return: nothing
+*/
+void set_string(char **s, char *to)
+{
+	char *tmp = (char *)*s;
+	to = tmp;
+	to++;
+}
