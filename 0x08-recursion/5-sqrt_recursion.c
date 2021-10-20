08cbb8b69363d0b730f3dcc32269531f1c2b5bf1
ozswar94 <3630@holbertonschool.com>
2021-10-20 18:18:38 +0000

    finish task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* _sqrt - square root tmp
+* @base: int
+* @x: int
+* Return: On success, these functions return the square root of base.
+*/
+int _sqrt(int base, int x)
+{
+	if (x * x == base)
+		return (x);
+	if (x * x > base)
+		return (-1);
+	return _sqrt(base, x + 1);
+
+}
+
+/**
+* _sqrt_recursion - These functions return the nonnegative square root of x
+* @n: 
+* Return: On success, these functions return the square root of x.
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	return (_sqrt(n, 1));
+}
