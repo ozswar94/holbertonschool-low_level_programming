46ad458dd7172c722ef31fb84b1dde1830187f15
ozswar94 <3630@holbertonschool.com>
2021-10-20 20:25:24 +0000

    add last mandatory

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
 * _sqrt - square root tmp
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* prime - check if n is prime
+* @n: int
+* Return: 1 if is prime 0 if not
+*/
+int _prime(int n, int p)
+{
+	if (p <= 1)
+		return (0);
+	else if (p == 2)
+		return (1);
+	else if (n % p == 0)
+		return (0);
+	return (_prime(n, p - 1));
+}
+
+/**
+* is_prime_number - check if n is prime
+* @n: int
+* Return: 1 if is prime 0 if not
+*/
+int is_prime_number(int n)
+{
+	return (_prime(n, n - 1));
+}
