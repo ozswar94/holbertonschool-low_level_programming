74fdc26ede11b7a0531fdc5f965ac12f493ebf11
ozswar94 <3630@holbertonschool.com>
2021-10-20 12:26:18 +0000

    fix 0 bytes

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,11 +8,11 @@
 */
 void _print_rev_recursion(char *s)
 {
-	if (s == NULL)
-		exit(1);
-	if (*(s + 1) != '\0')
+	if (*s != '\0')
 	{
 		_print_rev_recursion(s + 1);
 	}
+	if (*s == '\0')
+		return;
 	_putchar(*s);
 }
