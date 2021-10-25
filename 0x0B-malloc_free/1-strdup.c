6cd3bb6dc7a6d45ea78a0adc4214ec48e61566fc
ozswar94 <3630@holbertonschool.com>
2021-10-25 23:17:38 +0000

    add new task

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _strdup - create a new string and copy str in the new string
+* @str: string
+* Return: new string
+*/
+
+char *_strdup(char *str)
+{
+	unsigned int i;
+	unsigned int size;
+	char *buffer;
+
+	for (size = 0; str[size] != '\0' ; size++)
+	;
+	buffer = (char *)malloc(sizeof(char) * size);
+	if (!buffer || size == 0)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		buffer[i] = str[i];
+	return (buffer);
+}
+
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -4,6 +4,7 @@
 
 /*void _putchar(int c);*/
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 
 #endif /* MAIN_H */
 
