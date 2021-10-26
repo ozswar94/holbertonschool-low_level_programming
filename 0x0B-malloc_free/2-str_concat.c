14cc7e0d3ef243abef405d853b8600499c34e350
ozswar94 <3630@holbertonschool.com>
2021-10-26 07:47:52 +0000

    add task 2

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* str_concat - concat two string and return in new string
+* @s1: string
+* @s2: string
+* Return: concat string
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+	char *concat;
+	unsigned int i;
+	unsigned int j;
+	unsigned int size_s1;
+	unsigned int size_s2;
+	unsigned int size_concat;
+
+	for (size_s1 = 0; s1[size_s1] != '\0'; size_s1++)
+	;
+	for (size_s2 = 0; s1[size_s2] != '\0'; size_s2++)
+	;
+	size_concat = size_s1 + size_s2;
+
+	concat = (char *)malloc(sizeof(char) * size_concat + 1);
+	if (!concat)
+		return (NULL);
+	
+	for (i = 0; i < size_s1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j <= size_s2; j++)
+	{
+		concat[i] = s2[j];
+		i++;
+	}
+	return (concat);
+}
+
