b5e62033ab4c33623f04ae6295e74b303989cc09
ozswar94 <3630@holbertonschool.com>
2021-10-26 07:50:51 +0000

    fix and test task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
 * str_concat - concat two string and return in new string
 * @s1: string
@@ -23,9 +23,9 @@ char *str_concat(char *s1, char *s2)
 	size_concat = size_s1 + size_s2;
 
 	concat = (char *)malloc(sizeof(char) * size_concat + 1);
-	if (!concat)
+	if (!concat || !s1 || !s2)
 		return (NULL);
-	
+
 	for (i = 0; i < size_s1; i++)
 		concat[i] = s1[i];
 	for (j = 0; j <= size_s2; j++)
