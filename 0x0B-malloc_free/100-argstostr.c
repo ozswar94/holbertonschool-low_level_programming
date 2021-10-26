87e124c7e489cabe05de887907bcc152ec6979a2
ozswar94 <3630@holbertonschool.com>
2021-10-26 12:21:41 +0000

    add task 100

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* argstostr - priint all argument + name of program
+* @ac: number of argument + name of program
+* @av: argument + name of program
+* Return: nothing
+*/
+
+char *argstostr(int ac, char **av)
+{
+	int i, j, k, size;
+	char *buffer;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	size = 0;
+	for (i = 0; av[i] != NULL; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		;
+		size += j + i;
+	}
+
+	buffer = (char *)malloc(sizeof(char) * size + 1);
+	if (buffer == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; av[i] != NULL; i++)
+	{
+
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			buffer[k] = av[i][j];
+			k++;
+		}
+		buffer[k] = '\n';
+		k++;
+	}
+	buffer[k++] = '\0';
+	return (buffer);
+}
