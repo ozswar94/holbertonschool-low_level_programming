e4b208eefb6b062c63462f09cbec50249c6e0a27
ozswar94 <3630@holbertonschool.com>
2021-10-26 11:36:36 +0000

    fix free

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,7 +23,12 @@ int **alloc_grid(int width, int height)
 	{
 		new_tab[i] = (int *)malloc(sizeof(int) * width);
 		if (new_tab[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(new_tab[j]);
+			free(new_tab);
 			return (NULL);
+		}
 		for (j = 0; j < width; j++)
 			new_tab[i][j] = 0;
 	}
