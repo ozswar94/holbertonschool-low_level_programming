db187138b5b47bfcfdaf372ef76140416d719c86
ozswar94 <3630@holbertonschool.com>
2021-10-26 10:36:09 +0000

    fix all error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,14 @@
 #include "main.h"
+#include <stdio.h>
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
 
 /**
 * str_concat - concat two string and return in new string
@@ -10,37 +20,28 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *concat;
-	unsigned int i;
-	unsigned int j;
-	unsigned int size_s1;
-	unsigned int size_s2;
-	unsigned int size_concat;
+	unsigned int i, j;
+	unsigned int size_s1, size_s2 = 0;
+	unsigned int size_concat = 0;
 
-	for (size_s1 = 0; s1[size_s1] != '\0'; size_s1++)
-	;
-	for (size_s2 = 0; s2[size_s2] != '\0'; size_s2++)
-	;
+	if (s1 == NULL)
+		size_s1 = 0;
+	else
+		size_s1 = _strlen(s1);
+	if (s2 == NULL)
+		size_s2 = 0;
+	else
+		size_s2 = _strlen(s2);
 	size_concat = size_s1 + size_s2;
 
-	concat = (char *)malloc(sizeof(char) * size_concat);
+	concat = (char *)malloc(sizeof(char) * size_concat + 1);
 	if (concat == NULL)
 		return (NULL);
 
-	i = 0;
-	j = 0;
-	while (i + j < size_concat)
-	{
-		if (i < size_s1)
-		{
-			concat[i + j] = s1[i];
-			i++;
-		}
-		else
-		{
-			concat[i + j] = s2[j];
-			j++;
-		}
-	}
+	for (i = 0; i < size_s1; i++)
+		concat[i] = s1[i];
+	for (j = 0; j < size_s2; j++)
+		concat[i + j] = s2[j];
 	return (concat);
 }
 
