c7aab36f25edf67178dbd26eb4ebcd300364d2ea
Jordan Mpounza <3630@holbertonschool.com>
2021-10-28 12:13:33 +0000

    add task 100

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -12,14 +12,31 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	unsigned int i;
 	unsigned char *buffer;
+	unsigned char *ptr_tmp;
 
 	if (old_size == new_size)
 		return (ptr);
 	if (ptr == NULL)
 	{
+		ptr = (unsigned char *)malloc(new_size);
+		if (ptr == NULL)
+			return (NULL);
+		return (ptr);
+	}
+	if (new_size == 0 && ptr != NULL)
+	{
 		free(ptr);
 		return (NULL);
 	}
-	
+	ptr_tmp = (unsigned char *)ptr;
+	buffer = (unsigned char *)malloc(new_size);
+	if (buffer == NULL)
+		return (NULL);
+
+	for (i = 0; i < old_size; i++)
+		*(buffer + i) = *(ptr_tmp + i);
+	free(ptr);
+
+	return ((void *)buffer);
 }
 
