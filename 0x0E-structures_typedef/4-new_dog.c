4526ef2aa69544baa83af2ddaf006bbe0e82ee4d
ozswar94 <3630@holbertonschool.com>
2021-10-31 10:42:17 +0000

    test 2 task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,23 @@
 #include "dog.h"
 
 /**
+* _strlen - determine se sizeof ol string
+* @s: string
+* Return: the size of str
+*/
+
+unsigned int _strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+
+/**
 * new_dog - creat a dog
 * @name: name dog
 * @age: age of dog
@@ -10,14 +27,26 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	int i;
 	dog_t *d;
 
 	d = (dog_t *)malloc(sizeof(dog_t));
 	if (d == NULL)
 		return (NULL);
-	d->name = name;
+
+	d->name = (char *)malloc(sizeof(char) * _strlen(name) + 1);
+	if (d->name == NULL)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+		d->name[i] = name[i];
+	d->name[i] = '\0';
 	d->age = age;
-	d->owner = owner;
+	d->owner = (char *)malloc(sizeof(char) * _strlen(owner) + 1);
+	if (d->owner == NULL)
+		return (NULL);
+	for (i = 0; owner[i] != '\0'; i++)
+		d->owner[i] = owner[i];
+	d->owner[i] = '\0';
 
 	return (d);
 }
