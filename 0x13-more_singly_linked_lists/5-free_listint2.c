bd3453cb276027143b03a6f955c74bbacaa43cff
Jordan Mpounza <3630@holbertonschool.com>
2021-11-16 08:43:00 +0000

    test 2 task 5 + rm main

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,15 +11,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if (*head == NULL)
-		return;
-
 	while (*head != NULL)
 	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = NULL;
-		*head = tmp;
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
 	}
-	tmp = NULL;
+	*head = NULL;
 }
