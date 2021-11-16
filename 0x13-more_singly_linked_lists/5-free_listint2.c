18e30407b81c05c062c3dd43a11a20444088fa86
Jordan Mpounza <3630@holbertonschool.com>
2021-11-16 08:54:25 +0000

    add gitignore and test task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,11 +11,14 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
+	if (*head == NULL)
+		return;
+
 	while (*head != NULL)
 	{
 		tmp = *head;
 		*head = (*head)->next;
 		free(tmp);
+		tmp = NULL;
 	}
-	*head = NULL;
 }
