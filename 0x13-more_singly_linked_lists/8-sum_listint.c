1b13b895c51412bd1e9b8447dc41388cd21351ca
Jordan Mpounza <3630@holbertonschool.com>
2021-11-16 08:26:35 +0000

    add task 8

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+* sum_listint - add all node of element
+* @head: head node
+* Return: sum of all emelent
+*/
+int sum_listint(listint_t *head)
+{
+    int sum = 0;
+    listint_t *tmp;
+
+    tmp = *head;
+    while (tmp != NULL)
+    {
+        sum += tmp->n;
+        tmp = tmp->next;
+    }
+    return (sum);
+}
