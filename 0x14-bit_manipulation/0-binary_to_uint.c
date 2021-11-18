11463fc8dfe3b0b2cacf7c77e8fa0c2c49c4edd5
Jordan Mpounza <3630@holbertonschool.com>
2021-11-18 14:23:28 +0000

    test 2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,25 +10,21 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int nb = 0, exp = 1;
-	int i = 0;
+	int len = 0;
 
 	if (b == NULL)
 		return (0);
 
-	while (b[i + 1])
-	{
-		if (b[i] != '0' && b[i] != '1')
-			return (0);
-		exp *= 2;
-		i++;
-	}
-	i = 0;
-	while (b[i])
-	{
-		nb += exp * (b[i] - '0');
-		i++;
-		exp /= 2;
-	}
+	while (b[len])
+		len++;
 
+	for (len -= 1; len >= 0; len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		nb += (b[len] - '0') * exp;
+		exp *= 2;
+	}
 	return (nb);
 }
