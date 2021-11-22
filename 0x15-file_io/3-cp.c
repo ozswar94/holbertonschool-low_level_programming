e52879c0ff0a5c5e214ef055634986881c55e9b8
ozswar94 <3630@holbertonschool.com>
2021-11-22 18:10:12 +0000

    test task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -34,37 +34,49 @@ int main(int argc, char *argv[])
 */
 void copy_file_to_other(const char *file_from, const char *file_to)
 {
-	int fd_from, fd_to, len;
+	int fd_from, fd_to, r = 1, w = 1;
 	char buf[SIZE];
 
 	clean_tab(buf);
 
 	fd_from = open(file_from, O_RDONLY);
-	if (fd_from < 0)
+	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
 		exit(98);
 	}
 
 	fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (fd_to < 0)
+	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
 		exit(99);
 	}
 
-	while ((len = read(fd_from, buf, SIZE)) > 0)
+	while (r)
 	{
-		write(fd_to, buf, len);
+		r = read(fd_from, buf, SIZE);
+		if (r == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+			exit(98);
+		}
+		w = write(fd_to, buf, r);
+		if (w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
+			exit(99);
+		}
+		clean_tab(buf);
 	}
 
-	if (close(fd_from) < 0)
+	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
 
-	if (close(fd_to) < 0)
+	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
