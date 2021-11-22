c17e23f2e142890d38fabf33080059fe5cf5530c
ozswar94 <3630@holbertonschool.com>
2021-11-22 21:04:25 +0000

    change algo task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void copy_file_to_other(const char *file_from, const char *file_to);
+void error_file(char *name, int num_error, int fd);
 
 /**
 * main - main function
@@ -13,65 +13,67 @@ void copy_file_to_other(const char *file_from, const char *file_to);
 */
 int main(int argc, char *argv[])
 {
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	copy_file_to_other(argv[1], argv[2]);
-	return (0);
-}
-
-/**
-* copy_file_to_other - copy the file into another
-* @file_from: path of file
-* @file_to: path of file
-*
-*/
-void copy_file_to_other(const char *file_from, const char *file_to)
-{
-	int fd_from, fd_to, r = 1, w = 1;
 	char buf[1024];
+	int fd_from, fd_to, r, w;
 
-	fd_from = open(file_from, O_RDONLY);
+	if (argc != 3)
+		error_file(NULL, 97, 0);
+
+	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-		exit(98);
-	}
+		error_file(argv[1], 98, 0);
 
-	fd_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
-		exit(99);
-	}
+		error_file(argv[2], 99, 0);
 
 	while ((r = read(fd_from, buf, 1024)) != 0)
 	{
 		if (r == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
-			exit(98);
-		}
+			error_file(argv[1], 98, 0);
 		w = write(fd_to, buf, r);
 		if (w == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", file_to);
-			exit(99);
-		}
+			error_file(argv[2], 99, 0);
 	}
 
 	if (close(fd_from) == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+		error_file(NULL, 100, fd_from);
 
 	if (close(fd_to) == -1)
+		error_file(NULL, 100, fd_to);
+
+	return (0);
+}
+
+/**
+* error_file - check error file
+* @name: path of file
+* @num_error: path of file
+* @fd:
+*
+*/
+void error_file(char *name, int num_error, int fd)
+{
+	switch (num_error)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(97);
+			break;
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", name);
+			exit(98);
+			break;
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", name);
+			exit(99);
+			break;
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+			exit(100);
+			break;
+		default:
+			break;
 	}
+
 }
