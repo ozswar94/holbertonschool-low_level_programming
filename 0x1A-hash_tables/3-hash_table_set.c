9fcaafe7d90733147e4386b0933a05c10f3c3293
Jordan Mpounza <3630@holbertonschool.com>
2022-01-28 11:45:35 +0000

    fix free

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -23,7 +23,10 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	if (key == NULL)
+	{
+		free(new);
 		return (0);
+	}
 	new->key = strdup(key);
 	new->value = strdup(value);
 	new->next = NULL;
